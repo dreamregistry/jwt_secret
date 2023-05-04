0c07541b330110dab1475bdf422096de33b4d989
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-05-04 12:39:21 +0200

    store secret in parameter store

--- main.tf
+++ main.tf
@@ -6,14 +6,36 @@ terraform {
       source  = "registry.terraform.io/hashicorp/random"
       version = "3.2.0"
     }
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "random" {}
+provider "aws" {}
+
+data "aws_region" "current" {}
 
 resource "random_uuid" "secret" {}
 
+resource "random_pet" "name" {
+
+}
+
+resource "aws_ssm_parameter" "secret" {
+  name        = "/jwt_secret/${random_pet.name.id}/value"
+  description = "JWT secret value"
+  type        = "SecureString"
+  value       = random_uuid.secret.result
+}
+
 output "JWT_SECRET" {
-  sensitive = true
-  value     = random_uuid.secret.result
+  value = {
+    type   = "ssm"
+    arn    = aws_ssm_parameter.secret.arn
+    key    = aws_ssm_parameter.secret.name
+    region = data.aws_region.current.name
+  }
 }
